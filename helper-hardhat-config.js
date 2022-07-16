7bbb210c64dab08595cbce38ca4bab4f89866b23
Sanskar Sharma <sanskarsharma1822@gmail.com>
2022-07-17 02:17:58 +0530

    modified helper-hardhat-config

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,17 +5,14 @@ const networkConfig = {
     },
     31337: {
         name: "localhost",
-        subscriptionId: "588",
         keepersUpdateInterval: "86400",
     },
     4: {
         name: "rinkeby",
-        subscriptionId: "6926",
         keepersUpdateInterval: "86400",
     },
     80001: {
         name: "rinkeby",
-        subscriptionId: "6926",
         keepersUpdateInterval: "86400",
     },
 }
