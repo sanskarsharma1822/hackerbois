2ebbb499d7180b52e1848d79dd0aa59155c726c7
Sanskar Sharma <sanskarsharma1822@gmail.com>
2022-07-28 22:05:52 +0530

    merging with updated Brand.sol

--- deploy/101-update-for-frontend-brands.js
+++ deploy/101-update-for-frontend-brands.js
@@ -7,7 +7,7 @@ module.exports = async function () {
     console.log("Updating brands abi ...")
     await updateAbi()
 }
-
+// Update abi
 const updateAbi = async function () {
     const brands = await ethers.getContract("Brands")
     fs.writeFileSync(BRANDS_ABI_FILE, brands.interface.format(ethers.utils.FormatTypes.json))
